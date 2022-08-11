55e11d341dfde746262b2a250375a93514496672
pooja <poojatambe445@gmail.com>
2022-08-11 16:47:44 -0700

    [pooja]Added:employee wage use case 8

---
+++ empWageUC8.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+partTime=1
+fullTime=2
+empRatePerHour=20
+maxHoursMonth=10
+numWorkingDays=20
+totalEmpHours=0
+totalWorkingDays=0
+
+function workHours() {
+
+         case $randomCheck in
+                $fullTime)
+                        empHours=16
+                        ;;
+                $partTime)
+                        empHours=8
+                        ;;
+                *)
+                        empHours=0
+                        ;;
+        esac
+        echo $empHours
+}
+
+while [[ $totalEmpHours -lt $maxHoursMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        randomCheck=$((RANDOM%3))
+        empHours=$( workHours $randomCheck )
+        totalEmpHours=$(($totalEmpHours+$empHours))
+        dailyWage[$totalWorkingDays]=$(($empHours*$empRatePerHour))
+
+done
+
+monthlyWage=$(($totalEmpHours+$empRatePerHour))
+
+echo ${dailyWage[@]}
